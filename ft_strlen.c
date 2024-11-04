a51e239987ce9860b9b638d6a04951c493cb61e6
Daniele Carretta <dacarret@car6s4.42barcelona.com>
2024-11-04 10:57:47 +0100

    LIBFT

--- ft_strlen.c
+++ ft_strlen.c
@@ -6,16 +6,18 @@
 /*   By: dacarret <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/20 13:11:46 by dacarret          #+#    #+#             */
-/*   Updated: 2024/09/20 13:23:11 by dacarret         ###   ########.fr       */
+/*   Updated: 2024/11/04 10:42:49 by dacarret         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
 
 size_t	ft_strlen(const char *s)
 {
-	int	x;
+	size_t	x;
 
 	x = 0;
+	if (!s)
+		return (0);
 	while (s[x] != '\0')
 		x++;
 	return (x);
